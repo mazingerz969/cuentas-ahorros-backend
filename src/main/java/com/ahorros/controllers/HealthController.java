052056c599d75e7cda97b31fa1d233f4237318fa
Mazingerz969 <billykantabro@gmail.com>
2025-07-04 20:13:28 +0200

    Configuración para Railway

---
+++ src/main/java/com/ahorros/controllers/HealthController.java
@@ -0,0 +1,13 @@
+package com.ahorros.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/actuator/health")
+    public String health() {
+        return "OK";
+    }
+} 
