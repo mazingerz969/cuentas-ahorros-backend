d7fab402ca0bfb00da2f134a9d7450e9dffa7391
Mazingerz969 <billykantabro@gmail.com>
2025-07-06 16:08:47 +0200

    chore: añadir usuario de ejemplo en data.sql para pruebas de login
    - Usuario: demo@demo.com
    - Contraseña: demo1234 (hash BCrypt)

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,3 +1,5 @@
 INSERT INTO usuarios (id, email, nombre, password, fecha_registro, activo)
 VALUES (100, 'demo@demo.com', 'Usuario Demo', '$2a$10$wH1Qw6Qw6Qw6Qw6Qw6Qw6OQw6Qw6Qw6Qw6Qw6Qw6Qw6Qw6Qw6', CURRENT_TIMESTAMP, true);
+
+
 -- Contraseña: demo1234 
